3e9d4c4db55c2d22f576c2e8bc1d9be6c2ca407a
Kowser <107254329+Kowser-Py-Dev@users.noreply.github.com>
2023-08-14 10:28:33 +0600

    Update GoToTop.jsx

--- src/components/GoToTop/GoToTop.jsx
+++ src/components/GoToTop/GoToTop.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./GoToTop.scss";
+import { BsArrowUp } from "react-icons/bs";
 
 const GoToTop = () => {
-   return <div>GoToTop</div>;
+   const [visible, setVisible] = useState(false);
+
+   const getOffSet = () => {
+      const offSetY = window.scrollY;
+      if (offSetY > 200) {
+         setVisible(true);
+      } else {
+         setVisible(false);
+      }
+   };
+
+   useEffect(() => {
+      window.addEventListener("scroll", getOffSet);
+   }, []);
+
+   const scrollToTop = () => {
+      window.scrollTo({
+         top: 0,
+         behavior: "smooth",
+      });
+   };
+   return (
+      <>
+         {visible ? (
+            <div className="go__to__top" onClick={scrollToTop}>
+               <BsArrowUp />
+            </div>
+         ) : (
+            ""
+         )}
+      </>
+   );
 };
 
 export default GoToTop;
